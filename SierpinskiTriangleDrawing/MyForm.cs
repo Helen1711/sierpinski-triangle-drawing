a81494044d9bcaa9ca9df28bd619cb2da91dc3c2
Helen1711 <helen.mysiak@gmail.com>
2019-04-02 15:23:22 +0300

    Добавьте файлы проекта.

---
+++ SierpinskiTriangleDrawing/MyForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SierpinskiTriangleDrawing
+{
+    public class MyForm : Form
+    {
+        private new void Move(ref int x, ref int y, int newX, int newY)
+        {
+            x = (newX + x) / 2;
+            y = (newY + y) / 2;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            int x = 100;
+            int y = 100;
+            var Rnd = new Random();
+            for (int i = 0; i < 100000; i++)
+            {
+                g.FillRectangle(Brushes.Purple, x, y, 1, 1);
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                var r = Rnd.Next(1, 30);
+                if (r < 10)
+                {
+                    Move(ref x, ref y, 100, 500);
+                }
+                else if (r < 20)
+                {
+                    Move(ref x, ref y, 600, 500);
+                }
+                else
+                {
+                    Move(ref x, ref y, 350, 100);
+                }
+            }
+            g.FillRectangle(Brushes.Aqua, x, y, 1, 1);
+        }
+    }
+}
---
+++ SierpinskiTriangleDrawing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SierpinskiTriangleDrawing
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Application.Run(new MyForm() { ClientSize = new Size(700, 700) });
+        }
+    }
+}
